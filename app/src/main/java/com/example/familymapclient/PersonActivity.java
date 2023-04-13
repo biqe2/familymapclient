fb9317da1c960350ffc3cc29db87eb72d850980d
biqe2 <biqe2@byu.edu>
2023-04-13 13:50:00 -0600

    it works to get the drop down lists. Problem is that they overlap to the other information

--- app/src/main/java/com/example/familymapclient/PersonActivity.java
+++ app/src/main/java/com/example/familymapclient/PersonActivity.java
@@ -6,8 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Person;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -27,6 +32,7 @@ public class PersonActivity extends AppCompatActivity {
     private PersonModel mother = null;
     private PersonModel child = null;
     private PersonModel spouse = null;
+    private PersonModel personSelected;
 
 
 
@@ -42,7 +48,7 @@ public class PersonActivity extends AppCompatActivity {
         ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
 
         DataCache data = DataCache.getInstance();
-        PersonModel personSelected = data.getPeople().get(selectedPersonID);
+        personSelected = data.getPeople().get(selectedPersonID);
 
         firstName.setText(personSelected.getFirstName());
         lastName.setText(personSelected.getLastName());
@@ -51,8 +57,11 @@ public class PersonActivity extends AppCompatActivity {
         } else{
             gender.setText("Male");
         }
+        //Generating the expandableListView
         Map<String, EventModel> events =  data.getEvents();
+
         List<EventModel> personEvents = new ArrayList<EventModel>();
+        List<PersonModel> personFamily = new ArrayList<PersonModel>();
 
         for(Map.Entry<String,EventModel> entry: events.entrySet()){
             EventModel event = entry.getValue();
@@ -65,25 +74,175 @@ public class PersonActivity extends AppCompatActivity {
 
         for(Map.Entry<String,PersonModel> entry: people.entrySet()){
             PersonModel person = entry.getValue();
-            String personGender = person.getGender();
+            //I might be able to make this one if statement with ors we will see.
             if(personSelected.getFatherID().equals(person.getPersonID())){
                 father = person;
+                personFamily.add(father);
             } else if(personSelected.getMotherID().equals(person.getPersonID())){
                 mother = person;
+                personFamily.add(mother);
             } else if(personSelected.getSpouseID().equals(person.getPersonID())){
                 spouse = person;
+                personFamily.add(spouse);
             } else if(selectedPersonID.equals(person.getFatherID()) || selectedPersonID.equals(person.getMotherID())){
                 child = person;
+                personFamily.add(child);
+            }
+        }
+        expandableListView.setAdapter(new ExpandableListAdapter(personFamily,personEvents));
+    }
+    private class ExpandableListAdapter extends BaseExpandableListAdapter{
+
+        private static final int PERSON_FAMILY_GROUP_POSITION = 1;
+        private static final int PERSON_EVENTS_GROUP_POSITION = 0;
+
+        private final List<PersonModel> personFamily;
+        private final List<EventModel> personEvents;
+
+        ExpandableListAdapter(List<PersonModel> personFamily, List<EventModel> personEvents){
+            this.personFamily = personFamily;
+            this.personEvents = personEvents;
+        }
+
+        @Override
+        public int getGroupCount() {
+            return 2;
+        }
+
+        @Override
+        public int getChildrenCount(int groupPosition) {
+            switch (groupPosition) {
+                case PERSON_FAMILY_GROUP_POSITION:
+                    return personFamily.size();
+                case PERSON_EVENTS_GROUP_POSITION:
+                    return personEvents.size();
+                default:
+                    throw new IllegalArgumentException("Unrecognized group position: " + groupPosition);
             }
         }
 
+        @Override
+        public Object getGroup(int groupPosition) {
+            return null;
+        }
+
+        @Override
+        public Object getChild(int groupPosition, int childPosition) {
+            return null;
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            if(convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.list_item_group, parent, false);
+            }
+
+            TextView titleView = convertView.findViewById(R.id.listTitle);
+
+            switch (groupPosition) {
+                case PERSON_FAMILY_GROUP_POSITION:
+                    titleView.setText("FAMILY");
+                    break;
+                case PERSON_EVENTS_GROUP_POSITION:
+                    titleView.setText("LIFE EVENTS");
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unrecognized group position: " + groupPosition);
+            }
+
+            return convertView;
+        }
+
+        @Override
+        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            View itemView;
+            switch(groupPosition) {
+                case PERSON_FAMILY_GROUP_POSITION:
+                    itemView = getLayoutInflater().inflate(R.layout.expandable_list_person_activity, parent, false);
+                    initializeExpandableListFamily(itemView, childPosition);
+                    break;
+                case PERSON_EVENTS_GROUP_POSITION:
+                    itemView = getLayoutInflater().inflate(R.layout.expandable_list_person_activity, parent, false);
+                    initializeExpandableListEvents(itemView, childPosition);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unrecognized group position: " + groupPosition);
+            }
+            return itemView;
+        }
+
+        private void initializeExpandableListFamily(View familyView, final int childPosition) {
+            PersonModel familyMember = personFamily.get(childPosition);
+
+            ImageView img = (ImageView) familyView.findViewById(R.id.iconImageView);
+            if(familyMember.getGender().equals("f")){
+                img.setImageResource(R.drawable.femaleicon);
+            } else{
+                img.setImageResource(R.drawable.maleicon);
+            }
+            TextView textTop = familyView.findViewById(R.id.textTop);
+            TextView textBottom = familyView.findViewById(R.id.textBottom);
+            textTop.setText(familyMember.getFirstName() + " " + familyMember.getLastName());
+
+            if(personSelected.getFatherID().equals(familyMember.getPersonID())){
+                textBottom.setText("Father");
+            } else if(personSelected.getMotherID().equals(familyMember.getPersonID())){
+                textBottom.setText("Mother");
+            } else if(personSelected.getSpouseID().equals(familyMember.getPersonID())){
+                textBottom.setText("Spouse");
+            } else if(selectedPersonID.equals(familyMember.getFatherID()) || selectedPersonID.equals(familyMember.getMotherID())){
+                textBottom.setText("Child");
+            }
+
+
+          /*  familyView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(PersonActivity.this, getString(R.string.skiResortToastText, skiResorts.get(childPosition).getName()), Toast.LENGTH_SHORT).show();
+                }
+            });*/
+        }
+
+        private void initializeExpandableListEvents(View eventView, final int childPosition) {
+            EventModel eventPerson = personEvents.get(childPosition);
+
+            ImageView img = (ImageView) eventView.findViewById(R.id.iconImageView);
+            img.setImageResource(R.drawable.markericon);
+            TextView textTop = eventView.findViewById(R.id.textTop);
+            TextView textBottom = eventView.findViewById(R.id.textBottom);
+            textTop.setText(eventPerson.getEventType().toUpperCase() + ": " + eventPerson.getCity() +
+                    ", " + eventPerson.getCountry() + " (" + eventPerson.getYear() + ")");
+            textBottom.setText(personSelected.getFirstName() + " " + personSelected.getLastName());
 
 
 
 
+          /*  familyView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(PersonActivity.this, getString(R.string.skiResortToastText, skiResorts.get(childPosition).getName()), Toast.LENGTH_SHORT).show();
+                }
+            });*/
+        }
 
-
-
-
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
+            return true;
+        }
     }
 }
